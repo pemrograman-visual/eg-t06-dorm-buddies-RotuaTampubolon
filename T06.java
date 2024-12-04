c0a84c32e720cf5f2213c538d7d57d891a99d574
RotuaTampubolon <rotuaimmanuela@gmail.com>
2024-12-04 16:08:54 +0700

    Pair 01

--- T06.java
+++ T06.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T06 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
